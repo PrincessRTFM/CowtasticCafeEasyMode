0e3534d0ee07f8412a2de015282a74b4fe6b0228
Lilith <lsong@princessrtfm.com>
2024-01-07 06:34:40 -0500

    Add keybinds to change money by 100 and 1000 at once
    Requested by SNBCL360 on the itch.io thread.

--- CowtasticEasyMode/Patches/Money.cs
+++ CowtasticEasyMode/Patches/Money.cs
@@ -9,19 +9,41 @@ using PrincessRTFM.CowtasticCafeEasyMode.Logging;
 using UnityEngine;
 
 [HarmonyPatch]
+[SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "delegate conformation")]
 internal class Money {
 	[HotkeyTrigger("Remove $10", KeyCode.PageDown)]
-	[SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "delegate conformation")]
-	public static void RemoveMoney(KeyCode trigger) {
+	public static void RemoveMoney10(KeyCode trigger) {
 		float amount = Mathf.Min(10, BaseGameMode.instance.Money);
 		BaseGameMode.instance.Money -= amount;
 		Log.Info($"Removed ${amount} (now at ${BaseGameMode.instance.Money})");
 	}
+	[HotkeyTrigger("Remove $100", KeyCode.End)]
+	public static void RemoveMoney100(KeyCode trigger) {
+		float amount = Mathf.Min(100, BaseGameMode.instance.Money);
+		BaseGameMode.instance.Money -= amount;
+		Log.Info($"Removed ${amount} (now at ${BaseGameMode.instance.Money})");
+	}
+	[HotkeyTrigger("Remove $1000", KeyCode.Delete)]
+	public static void RemoveMoney1000(KeyCode trigger) {
+		float amount = Mathf.Min(1000, BaseGameMode.instance.Money);
+		BaseGameMode.instance.Money -= amount;
+		Log.Info($"Removed ${amount} (now at ${BaseGameMode.instance.Money})");
+	}
+
 	[HotkeyTrigger("Add $10", KeyCode.PageUp)]
-	[SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "delegate conformation")]
-	public static void AddMoney(KeyCode trigger) {
+	public static void AddMoney10(KeyCode trigger) {
 		BaseGameMode.instance.Money += 10;
 		Log.Info($"Added $10 (now at ${BaseGameMode.instance.Money})");
+	}
+	[HotkeyTrigger("Add $100", KeyCode.Home)]
+	public static void AddMoney100(KeyCode trigger) {
+		BaseGameMode.instance.Money += 100;
+		Log.Info($"Added $100 (now at ${BaseGameMode.instance.Money})");
+	}
+	[HotkeyTrigger("Add $1000", KeyCode.Insert)]
+	public static void AddMoney1000(KeyCode trigger) {
+		BaseGameMode.instance.Money += 1000;
+		Log.Info($"Added $1000 (now at ${BaseGameMode.instance.Money})");
 	}
 
 	[HarmonyPrefix]
